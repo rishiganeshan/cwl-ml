32928d7b939bb2b4aa8be5e9b0733ba861dba0f7
rishiganeshan <k20004392@kcl.ac.uk>
2023-04-01 15:21:45 +0100

    Working deployment workflow

--- deploy.py
+++ deploy.py
@@ -19,12 +19,10 @@ def save_predictions_to_csv(file_name, model_name, x_random, y_pred):
 
 if __name__ == "__main__":
     import sys
-    print("YooooooooYooooooooYooooooooYooooooooYooooooooYooooooooYooooooooYooooooooYooooooooYooooooooYooooooooYooooooooYoooooooo")
 
     saved_models_dir = sys.argv[1]
     model_paths = [os.path.join(saved_models_dir, model) for model in os.listdir(saved_models_dir) if os.path.isdir(os.path.join(saved_models_dir, model))]
     print(f"Model paths: {model_paths}")
-    print("YooooooooYooooooooYooooooooYooooooooYooooooooYooooooooYooooooooYooooooooYooooooooYooooooooYooooooooYooooooooYoooooooo")
     for model_path in model_paths:
         model_name = os.path.basename(model_path)
         x_random, y_pred = make_predictions(model_path)
